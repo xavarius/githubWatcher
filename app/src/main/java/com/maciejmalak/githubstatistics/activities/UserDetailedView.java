7cf03f765e848a90f1ef0f0caf67c5af1873b3df
Maciej Malak <maciejmalak@gmail.com>
2016-02-06 23:58:05 +0100

    Model is changed to provide data as a GithubUser entry.

--- app/src/main/java/com/maciejmalak/githubstatistics/activities/UserDetailedView.java
+++ app/src/main/java/com/maciejmalak/githubstatistics/activities/UserDetailedView.java
@@ -5,14 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.maciejmalak.githubstatistics.R;
-import com.maciejmalak.githubstatistics.model.GithubStatisticModel;
+import com.maciejmalak.githubstatistics.model.GithubUser;
+import com.maciejmalak.githubstatistics.model.GithubUserModel;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class UserDetailedView extends AppCompatActivity {
 
-    final GithubStatisticModel mModel = GithubStatisticModel.getInstance();
+    final GithubUserModel mModel = GithubUserModel.getInstance();
 
     @Bind(R.id.detailed_user_name) protected TextView mTvUserName;
     @Bind(R.id.detailed_nbr_repos) protected TextView mTvNbrRepos;
@@ -29,7 +30,7 @@ public class UserDetailedView extends AppCompatActivity {
 
     private void loadDetailedUserData(final String userName) {
         mTvUserName.setText(userName);
-        final int reposAmout = mModel.getRepositoryListForUser(userName).size();
-        mTvNbrRepos.setText(String.format("%d",reposAmout));
+        final GithubUser user = mModel.getUser(userName);
+        mTvNbrRepos.setText(String.format("%d",user.getReposAmout()));
     }
 }
---
+++ app/src/main/java/com/maciejmalak/githubstatistics/model/GithubUserModel.java
@@ -0,0 +1,50 @@
+package com.maciejmalak.githubstatistics.model;
+
+import android.support.annotation.NonNull;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+// TODO store in a DB, sync with DB
+
+public class GithubUserModel {
+    private static GithubUserModel mModelInstance = null;
+    private static LinkedHashMap<String, GithubUser> mUsersData;
+
+    private GithubUserModel() {
+        mUsersData = new LinkedHashMap<>();
+    }
+
+    public static synchronized GithubUserModel getInstance() {
+        if (mModelInstance == null) {
+            mModelInstance = new GithubUserModel();
+        }
+        return mModelInstance;
+    }
+
+    public synchronized void putUser(final GithubUser user) {
+        if (!mUsersData.containsValue(user)) {
+            mUsersData.put(user.getUserLogin(), user);
+        }
+    }
+
+    public synchronized void putUserForce(final GithubUser user) {
+        mUsersData.put(user.getUserLogin(), user);
+    }
+
+    @NonNull
+    public synchronized LinkedHashMap<String, GithubUser> getAllUsers() {
+        return mUsersData;
+    }
+
+    @NonNull
+    public synchronized GithubUser getUser(final String user) {
+        for (Map.Entry<String, GithubUser> entry : mUsersData.entrySet()) {
+            if (entry.getKey().equals(user)) {
+                return entry.getValue();
+            }
+        }
+        return new GithubUser(Collections.<Repository>emptyList(),"");
+    }
+}
