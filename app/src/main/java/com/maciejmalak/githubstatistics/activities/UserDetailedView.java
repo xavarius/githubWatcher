c1d16bc04afffa68304bbc45c5119aec7c0ece69
Maciej Malak <maciej.malak@blstream.com>
2016-06-10 11:31:23 +0200

    Deleting Sofa library

--- app/src/main/java/com/maciejmalak/githubstatistics/activities/UserDetailedView.java
+++ app/src/main/java/com/maciejmalak/githubstatistics/activities/UserDetailedView.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.maciejmalak.githubstatistics.R;
-import com.maciejmalak.githubstatistics.model.GithubStatisticModel;
 import com.maciejmalak.githubstatistics.model.Owner;
 
 import butterknife.BindView;
@@ -13,12 +12,7 @@ import butterknife.ButterKnife;
 
 public class UserDetailedView extends AppCompatActivity {
 
-    final GithubStatisticModel mModel = GithubStatisticModel.getInstance();
-
     @BindView(R.id.detailed_user_name) protected TextView mUserName;
-//    @BindView(R.id.followers_on_github) protected TextView mFollowers;
-//    @BindView(R.id.starred) protected TextView mStarred;
-//    @BindView(R.id.following) protected TextView mFollowing;
     @BindView(R.id.detailed_nbr_repos) protected TextView mNbrRepos;
 
     @Override
@@ -27,15 +21,13 @@ public class UserDetailedView extends AppCompatActivity {
         setContentView(R.layout.activity_user_detailed_view);
 
         ButterKnife.bind(this);
-
-        loadDetailedUserData((Owner) getIntent().getExtras().getSerializable(UserDetailedView.class.getName()));
+        final Owner user = (Owner) getIntent().getExtras().getSerializable(UserDetailedView.class.getName());
+        if (user != null) loadDetailedUserData(user);
+//        else TODO
     }
 
     private void loadDetailedUserData(final Owner user) {
         mUserName.setText(user.getLogin());
-//        mFollowers.setText(user.getFollowers());
-//        mStarred.setText(user.getFollowing());
-//        mFollowing.setText(user.getFollowing());
-//        mNbrRepos.setText(user.getPublicRepos());
+        mNbrRepos.setText(user.getPublicRepos());
     }
 }
